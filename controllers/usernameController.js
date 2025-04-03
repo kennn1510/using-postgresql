0669f8cbce50b2b6c6c25480861f0f38cd64fe9d
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-04-03 09:09:21 -0500

    add database integration with PostgreSQL and dotenv configuration

--- controllers/usernameController.js
+++ controllers/usernameController.js
@@ -1,11 +1,19 @@
-exports.usernameGet = (req, res) => {
-  console.log("usernames will be stored here - wip");
+const db = require("../db/queries");
+require("dotenv").config();
+
+
+exports.usernameGet = async (req, res) => {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
 };
 
 exports.newUsernameGet = (req, res) => {
-  console.log("here is the input text field - wip");
+  res.render("new");
 };
 
-exports.newUsernamePost = (req, res) => {
-  console.log("username to be stored: ", req.body.username);
+exports.newUsernamePost = async (req, res) => {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
 };
---
+++ db/pool.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+console.log(process.env);
+
+// All of the following properties should be read from environment variables
+// We're hardcoding them here for simplicity
+module.exports = new Pool({
+  host: "localhost", // or wherever the db is hosted
+  user: process.env.role_name,
+  database: "top_users",
+  password: process.env.role_password,
+  port: 5432, // The default port
+});
